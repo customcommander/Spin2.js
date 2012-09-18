0e37ddde52333a3f716aa24ee8cad0dffa4afa92
customcommander <hello@spinjs.com>
2012-09-18 22:16:12 +0100

    Reset spin.moveTo specs

--- spec/spin.moveto.js
+++ spec/spin.moveto.js
@@ -1,8 +1,4 @@
 // Internally spin.moveTo uses spin.getPanel to match a panel corresponding
 // to "elt". This is thoroughly tested in spin.getpanel.js already.
 describe('spin.moveTo(elt)', function () {
-    it('Returns the corresponding panel', function () {
-        var home = spin.getPanel(0);
-        expect(spin.moveTo(0)).toBe(home);
-    });
 });
