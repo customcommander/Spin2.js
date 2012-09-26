8c09358e9796dd7146aa79a38d8d93e5d5e35c5b
customcommander <hello@spinjs.com>
2012-09-26 20:26:14 +0100

    Refactored use case and added/updated css rules
    - The use case is more sensible and cover all scenarios.
    - Added the last css animation rule that was missing.
    - Updated css animation rules to enhance the user experience

--- spec/spin.moveto.js
+++ spec/spin.moveto.js
@@ -59,6 +59,7 @@ describe('spin.moveTo(elt)', function () {
             waitsFor(pause(1000));
         });
 
+        // Case 1
         it('Home panel loads', function () {
             // Fakes a fresh start (as if the page just loaded)
             runs(function () {
@@ -85,6 +86,7 @@ describe('spin.moveTo(elt)', function () {
             });
         });
 
+        // Case 2
         it('Click on "Hammersmith & City Line"', function () {
 
             runs(function () {
@@ -109,6 +111,7 @@ describe('spin.moveTo(elt)', function () {
             });
         });
 
+        // Case 3
         it('Click on "Moorgate"', function () {
 
             runs(function () {
@@ -135,12 +138,71 @@ describe('spin.moveTo(elt)', function () {
             });
         });
 
+        // Case 4
+        it('Go back to "Hammersmith & City Line"', function () {
+
+            runs(function () {
+                spin.moveTo(panels.hammersmithAndCity);
+                expect(panels.home).toBecomeSmall('spin-hiddenleft');
+                expect(panels.hammersmithAndCity).toBecomeBig('spin-small');
+                expect(panels.moorgate).toBecomeHiddenRight('spin-big');
+            });
+
+            waitsFor(pause(2000));
+
+            runs(function () {
+                expect(panels.home).toBeSmall();
+                expect(panels.hammersmithAndCity).toBeBig();
+                expect(panels.moorgate).toBeHiddenRight();
+            });
+        });
+
+        // Case 5
+        it('Go back to Home', function () {
+
+            runs(function () {
+                spin.moveTo(panels.home);
+                expect(panels.home).toBecomeFull('spin-small');
+                expect(panels.hammersmithAndCity).toBecomeHiddenRight('spin-big');
+                expect(panels.moorgate).toBeHiddenRight();
+            });
+
+            waitsFor(pause(2000));
+
+            runs(function () {
+                expect(panels.home).toBeFull();
+                expect(panels.hammersmithAndCity).toBeHiddenRight();
+                expect(panels.moorgate).toBeHiddenRight();
+            });
+        });
+
+        // Case 6
+        it('Go back to "Moorgate"', function () {
+
+            runs(function () {
+                spin.moveTo(panels.moorgate);
+                expect(panels.home).toBecomeHiddenLeft('spin-full');
+                expect(panels.hammersmithAndCity).toBecomeSmall('spin-hiddenright');
+                expect(panels.moorgate).toBecomeBig('spin-hiddenright');
+            });
+
+            waitsFor(pause(2000));
+
+            runs(function () {
+                expect(panels.home).toBeHiddenLeft();
+                expect(panels.hammersmithAndCity).toBeSmall();
+                expect(panels.moorgate).toBeBig();
+            });
+        });
+
+        // Case 7
+        // We need to load a new panel beforehand
         it('Click on "Circle Line"', function () {
 
             runs(function () {
                 clickNav('circle');
             });
-            
+
             waitsFor(function () {
                 panels.circle = panels.moorgate.nextSibling;
                 return panels.circle;
@@ -163,175 +225,14 @@ describe('spin.moveTo(elt)', function () {
             });
         });
 
-        it('Click on "Monument"', function () {
-
-            runs(function () {
-                clickNav('monument');
-            });
-
-            waitsFor(function () {
-                panels.monument = panels.circle.nextSibling;
-                return panels.monument;
-            }, 'panel is not available', 1000);
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBecomeHiddenLeft('spin-small');
-                expect(panels.circle).toBecomeSmall('spin-big');
-                expect(panels.monument).toBecomeBig('spin-hiddenright');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeSmall();
-                expect(panels.monument).toBeBig();
-            });
-        });
-
-        it('Click on "Central Line"', function () {
-
-            runs(function () {
-                clickNav('central');
-            });
-
-            waitsFor(function () {
-                panels.central = panels.monument.nextSibling;
-                return panels.central;
-            }, 'panel is not available', 1000);
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBecomeHiddenLeft('spin-small');
-                expect(panels.monument).toBecomeSmall('spin-big');
-                expect(panels.central).toBecomeBig('spin-hiddenright');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeSmall();
-                expect(panels.central).toBeBig();
-            });
-        });
-
-        it('Click on "Holborn"', function () {
-
-            runs(function () {
-                clickNav('holborn');
-            });
-
-            waitsFor(function () {
-                panels.holborn = panels.central.nextSibling;
-                return panels.holborn;
-            }, 'panel is not available', 1000);
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBecomeHiddenLeft('spin-small');
-                expect(panels.central).toBecomeSmall('spin-big');
-                expect(panels.holborn).toBecomeBig('spin-hiddenright');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeHiddenLeft();
-                expect(panels.central).toBeSmall();
-                expect(panels.holborn).toBeBig();
-            });
-        });
-
-        it('Go to "Holborn"', function () {
-            runs(function () {
-                spin.moveTo(panels.holborn);
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeHiddenLeft();
-                expect(panels.central).toBeSmall();
-                expect(panels.holborn).toBeBig();
-            });
-        });
-
-        it('Go back to "Central Line"', function () {
-            runs(function () {
-                spin.moveTo(panels.central);
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBecomeSmall('spin-hiddenleft');
-                expect(panels.central).toBecomeBig('spin-small');
-                expect(panels.holborn).toBecomeHiddenRight('spin-big');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeSmall();
-                expect(panels.central).toBeBig();
-                expect(panels.holborn).toBeHiddenRight();
-            });
-        });
-
-        it('Go back to "Monument"', function () {
-            runs(function () {
-                spin.moveTo(panels.monument);
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBecomeSmall('spin-hiddenleft');
-                expect(panels.monument).toBecomeBig('spin-small');
-                expect(panels.central).toBecomeHiddenRight('spin-big');
-                expect(panels.holborn).toBeHiddenRight();
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeSmall();
-                expect(panels.monument).toBeBig();
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
-            });
-        });
-
         it('Go back to "Hammersmith & City Line"', function () {
+
             runs(function () {
                 spin.moveTo(panels.hammersmithAndCity);
                 expect(panels.home).toBecomeSmall('spin-hiddenleft');
                 expect(panels.hammersmithAndCity).toBecomeBig('spin-hiddenleft');
-                expect(panels.moorgate).toBeHiddenRight();
-                expect(panels.circle).toBecomeHiddenRight('spin-small');
-                expect(panels.monument).toBecomeHiddenRight('spin-big');
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
+                expect(panels.moorgate).toBecomeHiddenRight('spin-small');
+                expect(panels.circle).toBecomeHiddenRight('spin-big');
             });
 
             waitsFor(pause(2000));
@@ -341,22 +242,37 @@ describe('spin.moveTo(elt)', function () {
                 expect(panels.hammersmithAndCity).toBeBig();
                 expect(panels.moorgate).toBeHiddenRight();
                 expect(panels.circle).toBeHiddenRight();
-                expect(panels.monument).toBeHiddenRight();
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
             });
         });
 
-        it('Go back to home', function () {
+        // Case 8
+        it('Go to "Circle Line"', function () {
+            runs(function () {
+                spin.moveTo(panels.circle);
+                expect(panels.home).toBecomeHiddenLeft('spin-small');
+                expect(panels.hammersmithAndCity).toBecomeHiddenLeft('spin-big');
+                expect(panels.moorgate).toBecomeSmall('spin-hiddenright');
+                expect(panels.circle).toBecomeBig('spin-hiddenright');
+            });
+
+            waitsFor(pause(2000));
+
+            runs(function () {
+                expect(panels.home).toBeHiddenLeft();
+                expect(panels.hammersmithAndCity).toBeHiddenLeft();
+                expect(panels.moorgate).toBeSmall();
+                expect(panels.circle).toBeBig();
+            });
+        });
+
+        // Case 9
+        it('Go back to Home', function () {
             runs(function () {
                 spin.moveTo(panels.home);
-                expect(panels.home).toBecomeFull('spin-small');
-                expect(panels.hammersmithAndCity).toBecomeHiddenRight('spin-big');
-                expect(panels.moorgate).toBeHiddenRight();
-                expect(panels.circle).toBeHiddenRight();
-                expect(panels.monument).toBeHiddenRight();
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
+                expect(panels.home).toBecomeFull('spin-hiddenleft');
+                expect(panels.hammersmithAndCity).toBeHiddenRight();
+                expect(panels.moorgate).toBecomeHiddenRight('spin-small');
+                expect(panels.circle).toBecomeHiddenRight('spin-big');
             });
 
             waitsFor(pause(2000));
@@ -366,59 +282,6 @@ describe('spin.moveTo(elt)', function () {
                 expect(panels.hammersmithAndCity).toBeHiddenRight();
                 expect(panels.moorgate).toBeHiddenRight();
                 expect(panels.circle).toBeHiddenRight();
-                expect(panels.monument).toBeHiddenRight();
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
-            });
-        });
-
-        it('Go to "Holborn"', function () {
-            runs(function () {
-                spin.moveTo(panels.holborn);
-                expect(panels.home).toBecomeHiddenLeft('spin-full');
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeHiddenLeft();
-                expect(panels.central).toBecomeSmall('spin-hiddenright');
-                expect(panels.holborn).toBecomeBig('spin-hiddenright');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeHiddenLeft();
-                expect(panels.moorgate).toBeHiddenLeft();
-                expect(panels.circle).toBeHiddenLeft();
-                expect(panels.monument).toBeHiddenLeft();
-                expect(panels.central).toBeSmall();
-                expect(panels.holborn).toBeBig();
-            });
-        });
-
-        it('Go back to "Moorgate"', function () {
-            runs(function () {
-                spin.moveTo(panels.moorgate);
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBecomeSmall('spin-hiddenleft');
-                expect(panels.moorgate).toBecomeBig('spin-hiddenleft');
-                expect(panels.circle).toBeHiddenRight();
-                expect(panels.monument).toBeHiddenRight();
-                expect(panels.central).toBecomeHiddenRight('spin-small');
-                expect(panels.holborn).toBecomeHiddenRight('spin-big');
-            });
-
-            waitsFor(pause(2000));
-
-            runs(function () {
-                expect(panels.home).toBeHiddenLeft();
-                expect(panels.hammersmithAndCity).toBeSmall();
-                expect(panels.moorgate).toBeBig();
-                expect(panels.circle).toBeHiddenRight();
-                expect(panels.monument).toBeHiddenRight();
-                expect(panels.central).toBeHiddenRight();
-                expect(panels.holborn).toBeHiddenRight();
             });
         });
     }); // describe: Use Case
