152727b1fadd4c3b83419b45c534561604bd95d0
customcommander <hello@spinjs.com>
2012-10-06 16:57:12 +0100

    Refactored specs for spin

--- spec/spin.js
+++ spec/spin.js
@@ -1,38 +1,95 @@
 // Expected behaviour for spin()
 describe('spin([cfg])', function () {
 
-    // Note:
-    // We make pause during the tests in order to allow the animations to complete smoothly.
-    // Animations occur when we add or delete panels.
+    describe('cfg.content', function () {
 
-    it('Sets panel content', function () {
-        var panel;
-        runs(function () {
-            panel = spin({ content: '<p>Hello World!</p>' });
+        it('Sets panel content', function () {
+            runs(function () {
+                var panel   = spin({ content: '<p>Hello</p>' }),
+                    content = PanelHelper.getContent(panel);
+                expect(content).toBe('<p>Hello</p>');
+            });
+            // Waits for animation to finish
+            waitsFor(pause(2000));
         });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-        runs(function () {
-            expect(PanelHelper.getContent(panel)).toBe('<p>Hello World!</p>');
+
+        it('Defaults to an empty string', function () {
+            runs(function () {
+                var panel   = spin(),
+                    content = PanelHelper.getContent(panel);
+                expect(content).toBe('');
+            });
+            // Waits for animation to finish
+            waitsFor(pause(2000));
         });
-        runs(function () {
-            spin.deleteAfter(0);
+
+        it('Throws an error if content is not a string', function () {
+            var badCallErr = new Error('bad function call');
+
+            function badCall(cfg) {
+                return spin.bind({}, cfg);
+            }
+
+            expect(badCall({ content: undefined })).toThrow(badCallErr);
+            expect(badCall({ content: null })).toThrow(badCallErr);
+            expect(badCall({ content: true })).toThrow(badCallErr);
+            expect(badCall({ content: false })).toThrow(badCallErr);
+            expect(badCall({ content: 1 })).toThrow(badCallErr);
+            expect(badCall({ content: 0 })).toThrow(badCallErr);
+            expect(badCall({ content: -1 })).toThrow(badCallErr);
+            expect(badCall({ content: NaN })).toThrow(badCallErr);
+            expect(badCall({ content: Infinity })).toThrow(badCallErr);
+            expect(badCall({ content: -Infinity })).toThrow(badCallErr);
+            expect(badCall({ content: [] })).toThrow(badCallErr);
+            expect(badCall({ content: {} })).toThrow(badCallErr);
+            expect(badCall({ content: document.body })).toThrow(badCallErr);
+            expect(badCall({ content: function () {} })).toThrow(badCallErr);
+            expect(badCall({ content: new function () {} })).toThrow(badCallErr);
         });
-        waitsFor(pause(1500), 'Timeout!', 2000);
     });
 
-    it('Sets panel title', function () {
-        var panel;
-        runs(function () {
-            panel = spin({ title: 'Hello' });
+    describe('cfg.title', function () {
+        it('Sets panel title', function () {
+            runs(function () {
+                var panel = spin({ title: 'Hello' }),
+                    title = PanelHelper.getTitle(panel);
+                expect(title).toBe('Hello');
+            });
+            waitsFor(pause(2500));
         });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-        runs(function () {
-            expect(PanelHelper.getTitle(panel)).toBe('Hello');
+
+        it('Defaults to an empty string', function () {
+            runs(function () {
+                var panel = spin();
+                    title = PanelHelper.getTitle(panel);
+                expect(title).toBe('');
+            });
+            waitsFor(pause(2000));
         });
-        runs(function () {
-            spin.deleteAfter(0);
+
+        it('Throws an error if title is not a string', function () {
+            var badCallErr = new Error('bad function call');
+
+            function badCall(content, title) {
+                return spin.bind({}, content, title);
+            }
+
+            expect(badCall({ title: undefined })).toThrow(badCallErr);
+            expect(badCall({ title: null })).toThrow(badCallErr);
+            expect(badCall({ title: true })).toThrow(badCallErr);
+            expect(badCall({ title: false })).toThrow(badCallErr);
+            expect(badCall({ title: 1 })).toThrow(badCallErr);
+            expect(badCall({ title: 0 })).toThrow(badCallErr);
+            expect(badCall({ title: -1 })).toThrow(badCallErr);
+            expect(badCall({ title: NaN })).toThrow(badCallErr);
+            expect(badCall({ title: Infinity })).toThrow(badCallErr);
+            expect(badCall({ title: -Infinity })).toThrow(badCallErr);
+            expect(badCall({ title: [] })).toThrow(badCallErr);
+            expect(badCall({ title: {} })).toThrow(badCallErr);
+            expect(badCall({ title: document.body })).toThrow(badCallErr);
+            expect(badCall({ title: function () {} })).toThrow(badCallErr);
+            expect(badCall({ title: new function () {} })).toThrow(badCallErr);
         });
-        waitsFor(pause(1500), 'Timeout!', 2000);
     });
 
     it('Returns the panel that has been created', function () {
@@ -44,47 +101,6 @@ describe('spin([cfg])', function () {
         runs(function () {
             expect(panel).toBeDefined();
             expect(panel.parentNode).toBe(document.getElementById('spin-panels'));
-        });
-        runs(function () {
-            spin.deleteAfter(0);
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-    });
-
-    it('If not passed cfg is an empty object', function () {
-        runs(function () {
-            expect(function () { spin(); }).not.toThrow(new Error('bad function call'));
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-        runs(function () {
-            spin.deleteAfter(0);
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-    });
-
-    it('If not passed content is an empty string', function () {
-        var panel;
-        runs(function () {
-            panel = spin();
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-        runs(function () {
-            expect(PanelHelper.getContent(panel)).toBe('');
-        });
-        runs(function () {
-            spin.deleteAfter(0);
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-    });
-
-    it('If not passed title is an empty string', function () {
-        var panel;
-        runs(function () {
-            panel = spin();
-        });
-        waitsFor(pause(1500), 'Timeout!', 2000);
-        runs(function () {
-            expect(PanelHelper.getTitle(panel)).toBe('');
         });
         runs(function () {
             spin.deleteAfter(0);
@@ -111,51 +127,5 @@ describe('spin([cfg])', function () {
         expect(badCall([])).toThrow(badCallErr);
         expect(badCall(document.body)).toThrow(badCallErr);
         expect(badCall(function () {})).toThrow(badCallErr);
-    });
-
-    it('Throws an error if content is not a string', function () {
-        var badCallErr = new Error('bad function call');
-
-        function badCall(cfg) {
-            return spin.bind({}, cfg);
-        }
-
-        expect(badCall({ content: undefined })).toThrow(badCallErr);
-        expect(badCall({ content: null })).toThrow(badCallErr);
-        expect(badCall({ content: true })).toThrow(badCallErr);
-        expect(badCall({ content: false })).toThrow(badCallErr);
-        expect(badCall({ content: 1 })).toThrow(badCallErr);
-        expect(badCall({ content: 0 })).toThrow(badCallErr);
-        expect(badCall({ content: -1 })).toThrow(badCallErr);
-        expect(badCall({ content: NaN })).toThrow(badCallErr);
-        expect(badCall({ content: Infinity })).toThrow(badCallErr);
-        expect(badCall({ content: -Infinity })).toThrow(badCallErr);
-        expect(badCall({ content: [] })).toThrow(badCallErr);
-        expect(badCall({ content: {} })).toThrow(badCallErr);
-        expect(badCall({ content: document.body })).toThrow(badCallErr);
-        expect(badCall({ content: function () {} })).toThrow(badCallErr);
-    });
-
-    it('Throws an error if title is not a string', function () {
-        var badCallErr = new Error('bad function call');
-
-        function badCall(content, title) {
-            return spin.bind({}, content, title);
-        }
-
-        expect(badCall({ title: undefined })).toThrow(badCallErr);
-        expect(badCall({ title: null })).toThrow(badCallErr);
-        expect(badCall({ title: true })).toThrow(badCallErr);
-        expect(badCall({ title: false })).toThrow(badCallErr);
-        expect(badCall({ title: 1 })).toThrow(badCallErr);
-        expect(badCall({ title: 0 })).toThrow(badCallErr);
-        expect(badCall({ title: -1 })).toThrow(badCallErr);
-        expect(badCall({ title: NaN })).toThrow(badCallErr);
-        expect(badCall({ title: Infinity })).toThrow(badCallErr);
-        expect(badCall({ title: -Infinity })).toThrow(badCallErr);
-        expect(badCall({ title: [] })).toThrow(badCallErr);
-        expect(badCall({ title: {} })).toThrow(badCallErr);
-        expect(badCall({ title: document.body })).toThrow(badCallErr);
-        expect(badCall({ title: function () {} })).toThrow(badCallErr);
     });
 });
