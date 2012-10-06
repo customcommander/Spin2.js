2c6e618e074c603ea248f29ae091873ab58b974d
customcommander <hello@spinjs.com>
2012-10-06 23:12:31 +0100

    Added url error expectations

--- spec/spin.xhrloader.js
+++ spec/spin.xhrloader.js
@@ -28,4 +28,25 @@ describe('spin.xhrLoader(elt)', function (){
         expect(badCall(" ")).toThrow(badCallErr);
         expect(badCall("\n\t")).toThrow(badCallErr);
     });
+
+    it('Throws an error if elt has not url', function () {
+
+        var noUrlErr = new Error('element has no url'),
+            elts;
+
+        elts = document.createElement('ol');
+        elts.innerHTML = [
+            '<li id="test1">no data-url</li>',
+            '<li id="test2" data-url="">empty data-url</li>',
+            '<li id="test3" data-url="  ">white space only data-url</li>'
+        ].join('');
+
+        function noUrl(elt) {
+            return spin.xhrLoader.bind({}, elt);
+        }
+
+        expect(noUrl(elts.querySelector('#test1'))).toThrow(noUrlErr);
+        expect(noUrl(elts.querySelector('#test2'))).toThrow(noUrlErr);
+        expect(noUrl(elts.querySelector('#test3'))).toThrow(noUrlErr);
+    });
 });
