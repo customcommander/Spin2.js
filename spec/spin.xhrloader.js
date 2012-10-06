8a302d17e7b08af5b3d1de9feebdfbc80aa60cb5
customcommander <hello@spinjs.com>
2012-10-06 22:27:06 +0100

    Added error expectations

--- spec/spin.xhrloader.js
+++ spec/spin.xhrloader.js
@@ -1,3 +1,31 @@
 describe('spin.xhrLoader(elt)', function (){
 
+    it('Throws an error if elt is not an html element', function () {
+
+        var badCallErr = new Error('bad function call');
+
+        function badCall(arg) {
+            return spin.xhrLoader.bind({}, arg);
+        }
+
+        // Edge cases but easy to test...
+        expect(badCall()).toThrow(badCallErr);
+        expect(badCall(null)).toThrow(badCallErr);
+        expect(badCall(undefined)).toThrow(badCallErr);
+        expect(badCall(0)).toThrow(badCallErr);
+        expect(badCall(1)).toThrow(badCallErr);
+        expect(badCall(-1)).toThrow(badCallErr);
+        expect(badCall(true)).toThrow(badCallErr);
+        expect(badCall(false)).toThrow(badCallErr);
+        expect(badCall(NaN)).toThrow(badCallErr);
+        expect(badCall(Infinity)).toThrow(badCallErr);
+        expect(badCall(-Infinity)).toThrow(badCallErr);
+        expect(badCall([])).toThrow(badCallErr);
+        expect(badCall({})).toThrow(badCallErr);
+        expect(badCall(function () {})).toThrow(badCallErr);
+        expect(badCall(new function () {})).toThrow(badCallErr);
+        expect(badCall("")).toThrow(badCallErr);
+        expect(badCall(" ")).toThrow(badCallErr);
+        expect(badCall("\n\t")).toThrow(badCallErr);
+    });
 });
