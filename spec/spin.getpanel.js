1f2341ea0cb4d9a1855496a5f5473311844c1c93
customcommander <hello@spinjs.com>
2013-06-10 22:29:13 +0100

    rewrote specs for spin.getPanel

--- spec/spin.getpanel.js
+++ spec/spin.getpanel.js
@@ -1,60 +1,52 @@
-describe('spin.getPanel(elt)', function () {
+describe("spin.getPanel(elt)", function () {
 
-    it('Returns the corresponding panel', function () {
-
-        runs(AppHelper.restart);
-
-        // Waits for home panel to be available
-        waitsFor(AppHelper.getHome);
-
-        // Waits for all panels to stop moving
-        waitsFor(AppHelper.notMoving);
-
-        runs(function () {
-            var home = AppHelper.getHome();
-            var elt = home.querySelector('#hammersmithandcity');
-            expect(spin.getPanel(0)).toBe(home);
-            expect(spin.getPanel(home.id)).toBe(home);
-            expect(spin.getPanel('hammersmithandcity')).toBe(home);
-            expect(spin.getPanel(home)).toBe(home);
-            expect(spin.getPanel(elt)).toBe(home);
-        });
+    beforeEach(function () {
+        runs( AppHelper.restart );
+        waitsFor( AppHelper.notMoving );
     });
 
-    it('Throws an error if elt is not valid', function () {
-        var badCallErr = new Error('bad function call');
-
-        function badCall(arg) {
-            return spin.getPanel.bind({}, arg);
-        }
-
-        expect(badCall()).toThrow(badCallErr);
-        expect(badCall(null)).toThrow(badCallErr);
-        expect(badCall(undefined)).toThrow(badCallErr);
-        expect(badCall(false)).toThrow(badCallErr);
-        expect(badCall(true)).toThrow(badCallErr);
-        expect(badCall('')).toThrow(badCallErr);
-        expect(badCall('   ')).toThrow(badCallErr);
-        expect(badCall({})).toThrow(badCallErr);
-        expect(badCall([])).toThrow(badCallErr);
-        expect(badCall(function () {})).toThrow(badCallErr);
-        expect(badCall(Infinity)).toThrow(badCallErr);
-        expect(badCall(-Infinity)).toThrow(badCallErr);
-        expect(badCall(NaN)).toThrow(badCallErr);
+    it("should accept an element id within a panel", function () {
+        var panel = spin.getPanel('central');
+        expect(panel).toBe(document.querySelector('.spin-panel'));
     });
 
-    it('Throws an error if the corresponding panel is not found', function () {
+    it("should accept an element within a panel", function () {
+        var panel = spin.getPanel(document.querySelector("#central"));
+        expect(panel).toBe(document.querySelector('.spin-panel'));
+    });
 
-        var notFoundErr = new Error('panel not found');
+    it("should accept a panel id", function () {
+        var panel = spin.getPanel(document.querySelector('.spin-panel').id);
+        expect(panel).toBe(document.querySelector('.spin-panel'));
+    });
 
-        function notFound(arg) {
-            return spin.getPanel.bind({}, arg);
-        }
-        // These are valid calls but we expect them to fail
-        expect(notFound(-200)).toThrow(notFoundErr);
-        expect(notFound(200)).toThrow(notFoundErr);
-        expect(notFound('unknown_id')).toThrow(notFoundErr);
-        expect(notFound(document.body)).toThrow(notFoundErr);
-        expect(notFound(document.documentElement)).toThrow(notFoundErr);
+    it("should accept a panel", function () {
+        var panel = spin.getPanel(document.querySelector('.spin-panel'));
+        expect(panel).toBe(document.querySelector('.spin-panel'));
+    });
+
+    it("should accept a number", function () {
+        var panel = spin.getPanel(0);
+        expect(panel).toBe(document.querySelector('.spin-panel'));
+    });
+
+    it("should throw if elt is not valid", function () {
+        expect(function () { spin.getPanel([]); }).toThrow();
+    });
+
+    it("should throw if number is out of range", function () {
+        expect(function () { spin.getPanel(99); }).toThrow();
+    });
+
+    it("should throw if id is not within a panel", function () {
+        expect(function () { spin.getPanel('spin-nav'); }).toThrow();
+    });
+
+    it("should throw if element is not within a panel", function () {
+        expect(function () { spin.getPanel(document.body); }).toThrow();
+    });
+
+    it("should throw if id doesn't exist", function () {
+        expect(function () { spin.getPanel('unknown_id'); }).toThrow();
     });
 });
