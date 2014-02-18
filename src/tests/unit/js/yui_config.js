c7714d29797fe1c3be222256843b7fcbb6979b35
customcommander <hello@spinjs.com>
2014-02-18 23:43:26 +0000

    add a global yui modules config file and update test runners

---
+++ src/tests/unit/js/yui_config.js
@@ -0,0 +1,54 @@
+YUI_config = {
+    groups: {
+        // css development files
+        'spinjs-css': {
+            base: '../../css/',
+            modules: {
+                'spinjs-csscombo': {
+                    path: 'combo.css',
+                    type: 'css'
+                },
+                'spinjs-csscore': {
+                    path: 'spin.css',
+                    requires: ['spinjs-csscombo'],
+                    type: 'css'
+                }
+            }
+        },
+        // javascript development files
+        'spinjs-js': {
+            base: '../../js/',
+            modules: {
+                'spinjs-utils': {
+                    path: 'utils.js'
+                },
+                'spinjs-config': {
+                    path: 'config.js',
+                    requires: ['spinjs-utils']
+                },
+                'spinjs-core': {
+                    path: 'spin.js',
+                    requires: ['spinjs-csscore', 'spinjs-utils', 'spinjs-config']
+                }
+            }
+        },
+        // tests files
+        'spinjs-tests': {
+            base: './js/',
+            modules: {
+                'onload-tests': {
+                    path: 'onload-tests.js',
+                    requires: ['test', 'node', 'spinjs-core']
+                },
+                'utils-tests': {
+                    path: 'utils-tests.js',
+                    requires: ['test', 'node', 'spinjs-utils']
+                },
+                'config-tests': {
+                    path: 'config-tests.js',
+                    requires: ['test', 'node', 'spinjs-config']
+                }
+            }
+        }
+    }
+};
