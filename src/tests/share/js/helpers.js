ef84cac5e7c15f556072fe1195834be9984de859
customcommander <hello@spinjs.com>
2014-02-21 23:56:01 +0000

    define helpers for tests

---
+++ src/tests/share/js/helpers.js
@@ -0,0 +1,53 @@
+YUI.add('helpers-for-tests', function (Y) {
+
+Y.helpers = {
+
+    /**
+     * Waits until no panels are moving.
+     * @return {Promise} Resolve when no panels are moving.
+     */
+    waitUntilNothingMoves: function () {
+
+        return new Y.Promise(function (resolve) {
+
+            var wait,
+                css_anims = [
+                    'spin-small-hiddenleft',
+                    'spin-hiddenleft-small',
+                    'spin-big-hiddenleft',
+                    'spin-hiddenleft-big',
+                    'spin-full-hiddenleft',
+                    'spin-hiddenleft-full',
+                    'spin-small-hiddenright',
+                    'spin-hiddenright-small',
+                    'spin-big-hiddenright',
+                    'spin-hiddenright-big',
+                    'spin-full-hiddenright',
+                    'spin-hiddenright-full',
+                    'spin-big-small',
+                    'spin-small-big',
+                    'spin-full-small',
+                    'spin-small-full'
+                ];
+
+            function isMoving() {
+                return Y.all('.spin-panel').some(function (node) {
+                    return Y.Array.some(css_anims, function (klass) {
+                        return node.hasClass(klass);
+                    });
+                });
+            }
+
+            function check() {
+                if (!isMoving()) {
+                    wait.cancel();
+                    resolve();
+                }
+            }
+
+            wait = Y.later(50, null, check, [], true);
+        });
+    }
+};
+
+});
