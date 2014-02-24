9a3ddb569cdd54ac2b444930dc69ec4501236cef
customcommander <hello@spinjs.com>
2014-02-24 16:41:41 +0000

    remove duplicate in functional loading tests
    because both position and dimensions tests cover it already

--- src/tests/functional/js/onload-tests.js
+++ src/tests/functional/js/onload-tests.js
@@ -7,33 +7,28 @@ suite.add(new Y.Test.Case({
     name: "when the page loads",
 
     "the first panel should load": function () {
+
         var self = this;
+
         Y.helpers.waitUntilNothingMoves().then(function () {
             self.resume(function () {
                 Y.Assert.isNotNull(Y.one('.spin-panel'));
             });
         });
+
         this.wait();
     },
 
     "there should be only one panel": function () {
+
         var self = this;
+
         Y.helpers.waitUntilNothingMoves().then(function () {
             self.resume(function () {
                 Y.Assert.areSame(1, Y.all('.spin-panel').size());
             });
         });
-        this.wait();
-    },
 
-    "the first panel should take all the available space": function () {
-        var self = this;
-        Y.helpers.waitUntilNothingMoves().then(function () {
-            self.resume(function () {
-                var body = Y.one('body'), panel = Y.one('.spin-panel');
-                Y.Assert.areSame(body.getComputedStyle('width'), panel.getComputedStyle('width'));
-            });
-        });
         this.wait();
     }
 }));
