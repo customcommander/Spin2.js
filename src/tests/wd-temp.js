637d52802b4e2cdddbb7af996269f544856dd1b9
customcommander <hello@spinjs.com>
2015-04-12 00:52:31 +0100

    define browser when creating WebDriver instance

--- src/tests/wd-temp.js
+++ src/tests/wd-temp.js
@@ -3,7 +3,7 @@ describe('Spin', function () {
     var webdriver = require('selenium-webdriver');
 
     it('should do awesome stuff', function () {
-        var driver = new webdriver.Builder().build();
+        var driver = new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).build();
         driver.quit();
     });
 });
