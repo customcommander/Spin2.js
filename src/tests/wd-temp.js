d6ab9196879e4d15252b15507d3d252bd2a035f6
customcommander <hello@spinjs.com>
2015-04-12 00:57:51 +0100

    define remote url for webdriver tests

--- src/tests/wd-temp.js
+++ src/tests/wd-temp.js
@@ -3,7 +3,10 @@ describe('Spin', function () {
     var webdriver = require('selenium-webdriver');
 
     it('should do awesome stuff', function () {
-        var driver = new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).build();
+        var driver = new webdriver.Builder().
+                forBrowser(webdriver.Browser.CHROME).
+                usingServer('http://'+process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com/wd/hub').
+                build();
         driver.quit();
     });
 });
